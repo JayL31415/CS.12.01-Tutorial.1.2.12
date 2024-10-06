998f4199fe1049a943a37a61e811a37b4097c694
J <jaylauhc@gmail.com>
2024-10-06 18:51:01 +0800

    Completed

---
+++ src/Position.java
@@ -0,0 +1,16 @@
+public class Position {
+    private int a;
+    private int b;
+    public Position(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getR() {
+        return b;
+    }
+
+    public int getC() {
+        return a;
+    }
+}
